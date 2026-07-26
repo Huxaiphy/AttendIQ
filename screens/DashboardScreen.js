cfcc8e91238bfb4ced5b877212d73fe28c226cee
Huxaiphy <huzaifasalisu7@gmail.com>
2026-07-26 18:57:16 +0100

    adding dashboard

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,17 +1,48 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebaseConfig";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function DashboardScreen() {
 return (
-    <View style={styles.container}>
+    <View style={styles.menuContainer}>
         <Text style={styles.title}>Dashboard</Text>
         <Text style={styles.subtitle}>Welcome to the AttendIQ Dashboard</Text>
+
+            <TouchableOpacity 
+    style={styles.card}
+    onPress={() => navigation.navigate("Register")}
+    >
+    <Text style={styles.cardText}>Register Student</Text>
+    </TouchableOpacity>
+
+        <TouchableOpacity 
+    style={styles.card}
+    onPress={() => navigation.navigate("Attendance")}
+    >
+    <Text style={styles.cardText}>Take Attendance</Text>
+    </TouchableOpacity>
+
+        <TouchableOpacity 
+    style={styles.card}
+    onPress={() => navigation.navigate("Records")}
+    >
+    <Text style={styles.cardText}>Attendance Records</Text>
+    </TouchableOpacity>
+
+        <TouchableOpacity 
+        style={styles.logout}
+        onPress={async () => {
+        await signOut(auth);
+        navigation.replace("Login");}}>
+    <Text style={styles.logoutText}>Logout</Text>
+    </TouchableOpacity>
     </View>
 );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    menuContainer: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
